9fbb03c248043d16dc31932938acb9cc70e578f0
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-08-23 11:11:25 -0300

    Finished Task6

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -1,11 +1,14 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.dtos.ClientDTO;
+import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,13 +20,43 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @RequestMapping(path = "/clients", method = RequestMethod.POST)
+    public ResponseEntity<Object> register(
+
+            @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
+
+        if (firstName.isEmpty()) {
+            return new ResponseEntity<>("You must write down your first name", HttpStatus.FORBIDDEN);
+        }
+        if (lastName.isEmpty()) {
+            return new ResponseEntity<>("You must write down your last name", HttpStatus.FORBIDDEN);
+        }
+        if (email.isEmpty()) {
+            return new ResponseEntity<>("You must write down an email", HttpStatus.FORBIDDEN);
+        }
+        if (password.isEmpty()) {
+            return new ResponseEntity<>("You must write down a password", HttpStatus.FORBIDDEN);
+        }
+        if (clientRepository.findByEmail(email) != null) {
+            return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
+        }
+
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
     @RequestMapping("/clients")
     public List<ClientDTO> getClients() {
         return clientRepository.findAll().stream().map(ClientDTO::new).collect(Collectors.toList());
+    }
 
+    @RequestMapping("/clients/current")
+    public ClientDTO getClient(Authentication authentication){
+        return new ClientDTO(clientRepository.findByEmail(authentication.getName()));
     }
-    @RequestMapping("/clients/{id}")
-    public ClientDTO getClient(@PathVariable Long id){
-        return clientRepository.findById(id).map(ClientDTO::new).orElse(null);
-    }
+
 }
